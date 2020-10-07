e3d759637e29ec74bf20e154e4eefafa5538b6d5
Pedro Vivas <pmvp82@gmail.com>
2020-10-07 12:32:37 -0400

    first commit

---
+++ src/CurrencyRow.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function CurrencyRow({
+  currencyOptions,
+  selectedCurrency,
+  onChangeCurrency,
+  onChangeAmount,
+  amount,
+}) {
+  return (
+    <>
+      <input type="number" value={amount} onChange={onChangeAmount} />
+      <select value={selectedCurrency} onChange={onChangeCurrency}>
+        {currencyOptions.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </>
+  );
+}
